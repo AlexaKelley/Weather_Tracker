deed5c2f9ce950a06e95758b7d1ce849a2b6bab5
Alexa Kelley <34453376+AlexaKelley@users.noreply.github.com>
2020-05-26 22:01:05 -0400

    1st working version of the google function

---
+++ main.py
@@ -0,0 +1,43 @@
+from google.cloud import storage
+from google.cloud import secretmanager
+from datetime import datetime
+import os
+import json
+
+def gcs_writer(blob_text):
+    # Setting time and defining vars
+    time_var = datetime.now()
+    formated_time = time_var.strftime('"%Y-%m-%d - %H:%M"')
+    destination_blob_name = formated_time+"weather_tracking"
+    bucket_name = os.environ.get('bucket_name')
+    # Building client and writing the blob to bucket
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_string(blob_text)
+
+def weather_call():
+    import requests
+    #setting vars
+    project_id = os.environ.get('project_id')
+    # getting secrets
+    secrets = secretmanager.SecretManagerServiceClient()
+    wkey = secrets.access_secret_version("projects/"+project_id+"/secrets/wtracker_api_key/versions/1").payload.data.decode("utf-8")
+    # Define the call variables & construct api string
+    weather_api_key = wkey
+    zip_code = os.environ.get('zip_code')
+    country_code = os.environ.get('country_code')
+    api_call = "https://api.openweathermap.org/data/2.5/weather?zip="+zip_code+","+country_code+"&appid="+weather_api_key
+    # create request
+    response = requests.get(api_call)
+    response_json = json.loads(response.text)
+    exported_json = json.dumps(response_json)
+    return(exported_json)
+
+def main():
+    # make the api call
+    blob_text = weather_call()
+    # write the string to the bucket
+    gcs_writer(blob_text)
+
+main()
